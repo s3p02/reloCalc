c98fd1d8c85054c5c85897c2be6c8df9f6155e3b
Sree Sai Sharan Pantangi <sharan_sreesai@myhost.local>
2020-01-29 11:47:39 -0700

    added processed based parallelism to save requests

--- automate.py
+++ automate.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import sys
+from multiprocessing import Pool
 
 try:
     base_path = sys.argv[1]
@@ -15,10 +16,15 @@ def automate_calc(base_amount):
     ))
     return response.json()
 #print(automate_calc(7750.37))
-for i in range(7000, 15000, 1000):
+#for i in range(7000, 15000, 1000):
+def fetchNwrite(i):
     fname = base_path + '/' + '{}.json'.format(i)
     print(fname)
     a = automate_calc(i)
     print(a)
     with open(fname, 'w') as json_file:
         json.dump(a, json_file)
+walk = range(7000, 15000, 1000)
+p = Pool(5)
+p.map(fetchNwrite, walk)
+#https://docs.python.org/2/library/multiprocessing.html
